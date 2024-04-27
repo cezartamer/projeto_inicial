4f6fc44ee72bddceefc7f7bec867120a6138127d
Cezar Tamer[ <cezartamer@gmail.com>
2024-04-26 21:01:53 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Jornada de dados")
+
+#Variaveis
+
+idade = 36
+nome = "Cezar Tamer"
+
+#Print
+
+print(idade)
+
+#Soma
+
+NovaIdade = idade + 1
+print(NovaIdade)
+
+#Lista
+
+lista = [2,3,4,5,6]
+
+#Metodos
+
+lista_invertida = lista.reverse()
+print(lista_invertida)
